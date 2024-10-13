067b28f39905325661e458f43310a304c7bb73b7
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2024-10-12 20:08:12 -0400

    add project structure and AWS dependencies

---
+++ src/main/java/com/pharmaconnect/PharmaConnect360/PharmaConnect360Application.java
@@ -0,0 +1,13 @@
+package com.pharmaconnect.PharmaConnect360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaConnect360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaConnect360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmaconnect/PharmaConnect360/PharmaConnect360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmaconnect.PharmaConnect360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaConnect360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
